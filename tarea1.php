e099fc533abb99537ec70364b6000741cf653390
JuanPabloSarmiento <juanpnavarrete0@gmail.com>
2025-08-04 20:05:43 +0000

    'ready'

---
+++ tarea1.php
@@ -0,0 +1,90 @@
+$float1 =3.1 ;
+$float2 =3.2 ;
+$float3 =3.3 ;
+$float4 =3.4 ;
+$float5 =3.77 ;
+$int1 = 1;
+$int2 = 2;
+$int3 = 3;
+$int4 = 4;
+$int5 = 5;
+$bool1 = true;
+$bool2 = true;
+$bool3 =  false;
+$bool4 =  false;
+$bool5 =  false;
+$string1 = "buenas tardes niños" ;
+$string2 = "buenas tardes niñas" ;
+$string3 = "buenas tardes señores" ;
+$string4 = "buenas tardes señoritas" ;
+$string5 = "buenas tardes gente" ;
+
+$arreglo = [$float1,$float2,$float3,$float4,$float5,$bool1$bool2,$bool3,$bool4,$bool5,$int1,$int2,$int3,$int4,$int5,$string1,$string2,$string3,$string4,$string5];
+
+foreach ($variables as $arreglo){
+    echo $variables;
+}
+echo $arreglo[0];
+echo $arreglo[1];
+echo $arreglo[2];
+echo $arreglo[3];
+echo $arreglo[4];
+echo $arreglo[5];
+echo $arreglo[6];
+echo $arreglo[7];
+echo $arreglo[8];
+echo $arreglo[9];
+echo $arreglo[10];
+echo $arreglo[11];
+echo $arreglo[12];
+echo $arreglo[13];
+echo $arreglo[14];
+echo $arreglo[15];
+echo $arreglo[16];
+echo $arreglo[17];
+echo $arreglo[18];
+echo $arreglo[19];
+echo $arreglo[20];
+
+
+ $arrayAsosiativo1 = ["nombre" => "Juan", "edad" =>12  ];
+ $arrayAsosiativo2 = ["nombre" => "Pepe", "edad" =>11  ];
+ $arrayAsosiativo3 = ["nombre" => "Pedro", "edad" => 23 ];
+ $arrayAsosiativo4 = ["nombre" => "Alex", "edad" => 34 ];
+ $arrayAsosiativo5 = ["nombre" => "Maria", "edad" => 32];
+ $arrayAsosiativo6 = ["nombre" => "Mariana", "edad" => 22 ];
+ $arrayAsosiativo7 = ["nombre" => "Sonia", "edad" => 54 ];
+ $arrayAsosiativo8 = ["nombre" => "Jony", "edad" => 43 ];
+ $arrayAsosiativo9 = ["nombre" => "Sebastian", "edad" => 32 ];
+$arrayAsosiativo10 = ["nombre" => "Julia", "edad" => 32 ];
+$arrayAsosiativo11 = ["nombre" => "Julio", "edad" => 12 ];
+$arrayAsosiativo12 = ["nombre" => "Andres", "edad" => 15 ];
+$arrayAsosiativo13 = ["nombre" => "Carlos", "edad" => 76 ];
+$arrayAsosiativo14 = ["nombre" => "Jorge", "edad" => 45 ];
+$arrayAsosiativo15 = ["nombre" => "Ana", "edad" => 45 ];
+$arrayAsosiativo16 = ["nombre" => "Carol", "edad" => 76 ];
+$arrayAsosiativo17 = ["nombre" => "Carolina", "edad" => 45 ];
+$arrayAsosiativo18 = ["nombre" => "Liusa", "edad" =>  23];
+$arrayAsosiativo19 = ["nombre" => "Luis", "edad" => 25 ];
+$arrayAsosiativo20 = ["nombre" => "Tania", "edad" => 97 ];
+
+
+$base = [ $arrayAsosiativo1, $arrayAsosiativo2, $arrayAsosiativo3, $arrayAsosiativo4, $arrayAsosiativo5, $arrayAsosiativo6, $arrayAsosiativo7, $arrayAsosiativo8, $arrayAsosiativo9,$arrayAsosiativo10,$arrayAsosiativo11,$arrayAsosiativo12,$arrayAsosiativo13,$arrayAsosiativo14,$arrayAsosiativo15,$arrayAsosiativo16,$arrayAsosiativo17,$arrayAsosiativo18,$arrayAsosiativo19,$arrayAsosiativo20]
+
+$edadSuma=0;
+
+foreach ($objeto as $base){
+
+
+     $edadSuma += objeto[edad];
+
+}
+
+echo " el promedio de las edades es {$edadSuma / count($base)}"
+
+$arrayBidimensional = [[1,2],[3,4],[5,6],1,2],[3,4],[5,6],1,2],[3,4],[5,6],1,2],[3,4],[5,6],1,2],[3,4],[5,6]];
+
+foreach ($elemento as $arrayBidimensional){
+    echo elemento[1];
+    echo elemento[2];
+}
